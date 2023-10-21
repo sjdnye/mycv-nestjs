3b044156dabefaf5afc75e7f152bd24c794ad3a4
sjdnye <sjdnye@gmail.com>
2023-10-21 17:02:55 +0330

    implement TypeORM

--- src/reports/report.entity.ts
+++ src/reports/report.entity.ts
@@ -4,4 +4,7 @@ import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 export class Report {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
+  price: number;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,8 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 
-@Controller('users')
-export class UsersController {}
+import { CreateUserDto } from './dtos/create-user.dto';
+@Controller('auth')
+export class UsersController {
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {}
+}
