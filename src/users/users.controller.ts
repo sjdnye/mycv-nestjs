23aa31d9433d5712b1fe6ea9d67342dbcb9945c1
sajjad <sjdnye@gmail.com>
2023-12-11 14:57:12 +0330

    implement cookie & session

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,6 +11,7 @@ import {
   NotFoundException,
   UseInterceptors,
   ClassSerializerInterceptor,
+  Session,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -27,14 +28,40 @@ export class UsersController {
     private authService: AuthService,
   ) {}
 
+  // --------------------------- Example of cookie and session --------------------------
+  //   @Get('/colors/:color')
+  //   setColor(@Param("color") color: string, @Session() session: any){
+  //     session.color = color;
+  //   }
+
+  //   @Get('/colors')
+  //   getColor(@Session() session: any){
+  //     return session.color;
+  //   }
+  // ------------------------------------------------------------------------------------
+
+  @Get('/whoami')
+  whoAmI(@Session() session: any) {
+    return this, this.usersService.findOne(session.userId);
+  }
+
+  @Post('/signout')
+  signout(@Session() session: any) {
+    session.userId = null;
+  }
+
   @Post('/signup')
-  createUser(@Body() body: CreateUserDto) {
-    return this.authService.signup(body.email, body.password);
+  async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signup(body.email, body.password);
+    session.userId = user.id;
+    return user;
   }
 
   @Post('/signin')
-  signin(@Body() body: CreateUserDto) {
-    return this.authService.signin(body.email, body.password);
+  async signin(@Body() body: CreateUserDto) {
+    const user = await this.authService.signin(body.email, body.password);
+    sessionStorage.userId = user.id;
+    return user;
   }
 
   @Get('/:id')
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,45 +1,46 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import {Repository} from "typeorm"
-import {InjectRepository} from "@nestjs/typeorm"
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly repo: Repository<User>,
+  ) {}
 
-    constructor(
-        @InjectRepository(User)
-        private readonly repo: Repository<User>
-    ){}
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
 
-    create(email:string, password: string){
-        const user = this.repo.create({email, password});
+    return this.repo.save(user);
+  }
 
-        return this.repo.save(user);
+  findOne(id: number) {
+    if (!id) {
+      return null;
     }
+    return this.repo.findOneBy({ id });
+  }
 
-    findOne(id:number){
-        return this.repo.findOneBy({id})
+  find(email: string) {
+    return this.repo.find({ where: { email: email } });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('user is not found');
     }
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
 
-    find(email:string){
-      return this.repo.find({where:{email:email}})
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('user is not found');
     }
-
-   async update(id:number, attrs: Partial<User>){
-        const user = await this.findOne(id);
-        if(!user){
-            throw new NotFoundException('user is not found')
-        }
-        Object.assign(user,attrs);
-        return this.repo.save(user);
-    }
-
-    async remove(id:number){
-
-        const user = await this.findOne(id);
-        if(!user){
-            throw new NotFoundException('user is not found')
-        }
-        return this.repo.remove(user)
-    }
+    return this.repo.remove(user);
+  }
 }
